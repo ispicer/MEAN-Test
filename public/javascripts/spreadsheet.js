d07e44d2fa276c1cb061cf0f452f3c49f46d7af2
Isaiah Spicer <ispicer@temple.edu>
2016-01-30 16:43:34 -0500

    Rows and Columns can be deleted

--- public/javascripts/spreadsheet.js
+++ public/javascripts/spreadsheet.js
@@ -9,20 +9,33 @@ app.controller('mainController', function($scope) {
 			newRow.length = $scope.rows[0].length;
 		}
 		$scope.rows.push(newRow);
-		console.log($scope.rows.length);
-		console.log($scope.rows[0].length);
+		if($scope.rows[0].length == 0) {
+			$scope.rows[0].push('');
+		} 
 	}
 
 	$scope.addColumn = function() {
 		var newColumn = [];
-		newColumn.length = $scope.rows[0].length;
-		for(var i=0; i<$scope.rows.length; i++) {
-			$scope.rows[i].push(newColumn[i]);
+		if($scope.rows.length == 0) {
+			$scope.addRow();
+		} else {
+			newColumn.length = $scope.rows[0].length;
+			for(var i=0; i<$scope.rows.length; i++) {
+				$scope.rows[i].push(newColumn[i]);
+			}
 		}
 		console.log($scope.rows[0].length);
 	}
 
-	$scope.newData = function() {
+	$scope.deleteRow = function() {
+		if($scope.rows.length > 0) {
+			$scope.rows.pop();
+		}
+	}
 
+	$scope.deleteColumn = function() {
+		for(var i=0; i<$scope.rows.length; i++) {
+			$scope.rows[i].pop();
+		}
 	}
 })
